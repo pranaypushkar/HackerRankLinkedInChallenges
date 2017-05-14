e05174c8f190720d1bfa692853da82e4d1ca16cd
pranaypushkar <pranaypushkar92@gmail.com>
2017-05-14 23:20:46 +0530

    Add project files.

---
+++ HackerRankLinkedInChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankLinkedInChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
