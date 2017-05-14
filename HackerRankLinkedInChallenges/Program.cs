21fcb8fae27e725445c1d7a11639644ed989eed4
pranaypushkar <pranaypushkar92@gmail.com>
2017-05-14 23:23:57 +0530

    First Commit

--- HackerRankLinkedInChallenges/Program.cs
+++ HackerRankLinkedInChallenges/Program.cs
@@ -7,6 +7,7 @@ namespace HackerRankLinkedInChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
